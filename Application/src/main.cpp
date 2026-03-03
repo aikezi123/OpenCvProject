af03985bebc3fbb982f90ee9819c17f8c43cdb5c
yingqihang <yingqihang@femtomarch.com>
2026-03-03 10:41:47 +0800

    修改Application层架构

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
+    // 【新增核心代码】：手动初始化 UI 层的资源文件！
+    // 括号里的名字必须与你的 .qrc 文件名完全一致（你的文件叫 Resource.qrc）
+    Q_INIT_RESOURCE(Resource);
+
     MainWindow w;
     w.show();
 
